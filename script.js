9090896ac30b3407815306f34f2c6c9a99f9066d
Caio Schmidt França Fonseca <caiofonseca6@gmail.com>
2020-06-20 14:06:23 -0300

    JavaScript v2.0
    Consertei o bug que fazia o texto não ser traduzido quando feito em uma ordem específica de linguagem natural para binário e depois de binário para linguagem natural, o segundo simplesmente não funcionava mais.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 //Funcao converter para binario
 function converterparabin(){
+
+    const bin = null;
+
     //Pegar os valores da caixa de texto da esquerda
     const ling = document.getElementById('linguagem').value;
         if (ling === '') return alert('Coloque algum texto na caixa');
 
-    //Transformar texto em binario
+    //Transformar texto em binario e colocar na caixa da direita
     const caixa2 = document.getElementById('binario');
         caixa2.value = "";
         for (var i = 0; i < ling.length; i++){
@@ -13,24 +16,28 @@ function converterparabin(){
         }
 
     //Colocar resultado na caixa de texto da direita
-    const traduzido = document.getElementById('binario').innerHTML = ling.value;
-
+    // const traduzido = document.getElementById('binario').innerHTML = ling.value;
 
 }
+
 //Funcao converter para linguagem natural
 function converterparaling() {
+
+    const ling = null;
 
     //Pegar os valores da caixa de texto da direita
     const bin = document.getElementById('binario').value;
 
     //Transformar binario em texto
     if (bin === '') return alert('Coloque o codigo na caixa');
+    const teste = document.getElementById('linguagem');
 
     let outputStr = String.fromCharCode(
         ...bin.split(' ')
             .map(bino => parseInt(bino, 2))
     )
+    teste.value = outputStr;
 
     //Colocar resultado na caixa de texto da esquerda
-    const traduzido2 = document.getElementById('linguagem').innerHTML = outputStr;
+    // const traduzido2 = document.getElementById('linguagem').innerHTML = outputStr;
 }
